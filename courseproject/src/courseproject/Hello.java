9453768d866cbfd05cc02399190d9653a72e86ff
meghasravani95 <56423729+meghasravani95@users.noreply.github.com>
2020-09-21 19:53:43 -0400

    hello pgm

--- courseproject/src/courseproject/Hello.java
+++ courseproject/src/courseproject/Hello.java
@@ -1,9 +1,10 @@
+package courseproject;
 
-public class hei {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
 	}
 
 }
